d6ca9cecc407eba2ff8d7da69b24dfe15b2dc9b1
Ofir Landau <landauof@post.bgu.ac.il>
2019-02-21 11:53:52 +0200

    *evaluation unit - added a visualization of the models, accuracy score using kfold and a function to translate the clustering labeling *changed order of data-sets in json config file

--- Evaluation/Evaluator.py
+++ Evaluation/Evaluator.py
@@ -6,12 +6,36 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn import model_selection
 from sklearn.decomposition import PCA
+import itertools
+from sklearn.metrics import accuracy_score
 from sklearn import cluster, datasets, mixture
 from sklearn.neighbors import kneighbors_graph
 from sklearn.preprocessing import StandardScaler
 from itertools import cycle, islice
 
 from Evaluation.DataProcessor import *
+
+
+N_SPLITS = 10
+
+# Check all translations of labels to real labels and returns the best one
+def get_best_res(res, reference, fitness=accuracy_score):
+    labels = set(reference)
+    res_label = set(res)
+
+    num_of_labels_in_res = len(res_label)
+    best_res = list(res)
+    best_res_fitness = accuracy_score(best_res, reference)
+    # Iterate over permutations of real labels
+    for per in list(itertools.permutations(labels)):
+        curr_res = list(res)
+        for label_to_remove, label_to_insert in zip(res_label, per[:num_of_labels_in_res]):
+            curr_res[:] = [x if x != label_to_remove else label_to_insert for x in curr_res]
+        if accuracy_score(curr_res, reference) > best_res_fitness:
+            best_res = curr_res
+            best_res_fitness = accuracy_score(best_res, reference)
+
+    return best_res
 
 
 sns.set_context('poster')
@@ -23,9 +47,10 @@ dp.multiple_uci_to_csv()
 dp.manipulate_multiple_csv()
 data = dp.get_multiple_X_y_with_names()
 
-models = [('KMeans', cluster.KMeans)]
+models = [('KMeans', cluster.KMeans), ('MiniBatchKMeans', cluster.MiniBatchKMeans)]
 
 for key in data:
+    print("***** {} *****".format(key))
     X, y = data[key][0], data[key][1]
 
     pca = PCA(n_components=2)
@@ -36,267 +61,32 @@ for key in data:
 
     # TODO: Visualize using PCA (above) & find good scoring method
     for name, model in models:
-        kfold = model_selection.KFold(n_splits=10, random_state=7)
-        cv_results = model_selection.cross_val_score(model(n_clusters=len(list(set(y)))), X, y, cv=kfold, scoring='homogeneity_score')
-        results.append(cv_results)
-        names.append(name)
-        msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
-        print(msg)
+        kfold = model_selection.KFold(n_splits=N_SPLITS, random_state=7)
+        fitness_measures = {'accuracy': 0}
+        for train_index, test_index in kfold.split(principalComponents):
+            X_train, X_test = principalComponents[train_index], principalComponents[test_index]
+            y_train, y_test = y[train_index], y[test_index]
+            m = model(n_clusters=len(list(set(y))))
+            m.fit(X_train, y_train)
+            res = m.predict(X_test)
+            new_res = get_best_res(res, y_test)
+            fitness_measures['accuracy'] += accuracy_score(y_test, new_res)
+        for key in fitness_measures:
+            fitness_measures[key] = fitness_measures[key]/N_SPLITS
+        print("{}'s accuracy = {}".format(name, fitness_measures['accuracy']))
 
+        plt.subplot(1, 2, 1)
+        plt.scatter(principalComponents[:, 0], principalComponents[:, 1], c=y, cmap=plt.cm.Set1,
+                    edgecolor='k')
+        plt.subplot(1, 2, 2)
+        plt.scatter(principalComponents[:, 0], principalComponents[:, 1], c=m.fit_predict(X), cmap=plt.cm.Set1,
+                    edgecolor='k')
 
-# for i_dataset, (dataset, algo_params) in enumerate(datasets):
-#     # update parameters with dataset-specific values
-#     params = default_base.copy()
-#     params.update(algo_params)
-#
-#     X, y = dataset
-#
-#     # normalize dataset for easier parameter selection
-#     X = StandardScaler().fit_transform(X)
-#
-#     # estimate bandwidth for mean shift
-#     bandwidth = cluster.estimate_bandwidth(X, quantile=params['quantile'])
-#
-#     # connectivity matrix for structured Ward
-#     connectivity = kneighbors_graph(
-#         X, n_neighbors=params['n_neighbors'], include_self=False)
-#     # make connectivity symmetric
-#     connectivity = 0.5 * (connectivity + connectivity.T)
-#
-#     # ============
-#     # Create cluster objects
-#     # ============
-#     ms = cluster.MeanShift(bandwidth=bandwidth, bin_seeding=True)
-#     two_means = cluster.MiniBatchKMeans(n_clusters=params['n_clusters'])
-#     ward = cluster.AgglomerativeClustering(
-#         n_clusters=params['n_clusters'], linkage='ward',
-#         connectivity=connectivity)
-#     spectral = cluster.SpectralClustering(
-#         n_clusters=params['n_clusters'], eigen_solver='arpack',
-#         affinity="nearest_neighbors")
-#     dbscan = cluster.DBSCAN(eps=params['eps'])
-#     affinity_propagation = cluster.AffinityPropagation(
-#         damping=params['damping'], preference=params['preference'])
-#     average_linkage = cluster.AgglomerativeClustering(
-#         linkage="average", affinity="cityblock",
-#         n_clusters=params['n_clusters'], connectivity=connectivity)
-#     birch = cluster.Birch(n_clusters=params['n_clusters'])
-#     gmm = mixture.GaussianMixture(
-#         n_components=params['n_clusters'], covariance_type='full')
-#
-#     clustering_algorithms = (
-#         ('MiniBatchKMeans', two_means),
-#         ('AffinityPropagation', affinity_propagation),
-#         ('MeanShift', ms),
-#         ('SpectralClustering', spectral),
-#         ('Ward', ward),
-#         ('AgglomerativeClustering', average_linkage),
-#         ('DBSCAN', dbscan),
-#         ('Birch', birch),
-#         ('GaussianMixture', gmm)
-#     )
-#
-#     for name, algorithm in clustering_algorithms:
-#         t0 = time.time()
-#
-#         # catch warnings related to kneighbors_graph
-#         with warnings.catch_warnings():
-#             warnings.filterwarnings(
-#                 "ignore",
-#                 message="the number of connected components of the " +
-#                 "connectivity matrix is [0-9]{1,2}" +
-#                 " > 1. Completing it to avoid stopping the tree early.",
-#                 category=UserWarning)
-#             warnings.filterwarnings(
-#                 "ignore",
-#                 message="Graph is not fully connected, spectral embedding" +
-#                 " may not work as expected.",
-#                 category=UserWarning)
-#             algorithm.fit(X)
-#
-#         t1 = time.time()
-#         if hasattr(algorithm, 'labels_'):
-#             y_pred = algorithm.labels_.astype(np.int)
-#         else:
-#             y_pred = algorithm.predict(X)
-#
-#         plt.subplot(len(datasets), len(clustering_algorithms), plot_num)
-#         if i_dataset == 0:
-#             plt.title(name, size=18)
-#
-#         colors = np.array(list(islice(cycle(['#377eb8', '#ff7f00', '#4daf4a',
-#                                              '#f781bf', '#a65628', '#984ea3',
-#                                              '#999999', '#e41a1c', '#dede00']),
-#                                       int(max(y_pred) + 1))))
-#         # add black color for outliers (if any)
-#         colors = np.append(colors, ["#000000"])
-#         plt.scatter(X[:, 0], X[:, 1], s=10, color=colors[y_pred])
-#
-#         plt.xlim(-2.5, 2.5)
-#         plt.ylim(-2.5, 2.5)
-#         plt.xticks(())
-#         plt.yticks(())
-#         plt.text(.99, .01, ('%.2fs' % (t1 - t0)).lstrip('0'),
-#                  transform=plt.gca().transAxes, size=15,
-#                  horizontalalignment='right')
-#         plot_num += 1
-#
-# plt.show()
+        plt.show()
 
-
-
-
-
-
-
-
-
-
-# np.random.seed(0)
-#
-# # ============
-# # Generate datasets. We choose the size big enough to see the scalability
-# # of the algorithms, but not too big to avoid too long running times
-# # ============
-# n_samples = 1500
-# noisy_circles = datasets.make_circles(n_samples=n_samples, factor=.5,
-#                                       noise=.05)
-# noisy_moons = datasets.make_moons(n_samples=n_samples, noise=.05)
-# blobs = datasets.make_blobs(n_samples=n_samples, random_state=8)
-# no_structure = np.random.rand(n_samples, 2), None
-#
-# # Anisotropicly distributed data
-# random_state = 170
-# X, y = datasets.make_blobs(n_samples=n_samples, random_state=random_state)
-# transformation = [[0.6, -0.6], [-0.4, 0.8]]
-# X_aniso = np.dot(X, transformation)
-# aniso = (X_aniso, y)
-#
-# # blobs with varied variances
-# varied = datasets.make_blobs(n_samples=n_samples,
-#                              cluster_std=[1.0, 2.5, 0.5],
-#                              random_state=random_state)
-#
-# # ============
-# # Set up cluster parameters
-# # ============
-# plt.figure(figsize=(9 * 2 + 3, 12.5))
-# plt.subplots_adjust(left=.02, right=.98, bottom=.001, top=.96, wspace=.05,
-#                     hspace=.01)
-#
-# plot_num = 1
-#
-# default_base = {'quantile': .3,
-#                 'eps': .3,
-#                 'damping': .9,
-#                 'preference': -200,
-#                 'n_neighbors': 10,
-#                 'n_clusters': 3}
-#
-# datasets = [
-#     (noisy_circles, {'damping': .77, 'preference': -240,
-#                      'quantile': .2, 'n_clusters': 2}),
-#     (noisy_moons, {'damping': .75, 'preference': -220, 'n_clusters': 2}),
-#     (varied, {'eps': .18, 'n_neighbors': 2}),
-#     (aniso, {'eps': .15, 'n_neighbors': 2}),
-#     (blobs, {}),
-#     (no_structure, {})]
-#
-# for i_dataset, (dataset, algo_params) in enumerate(datasets):
-#     # update parameters with dataset-specific values
-#     params = default_base.copy()
-#     params.update(algo_params)
-#
-#     X, y = dataset
-#
-#     # normalize dataset for easier parameter selection
-#     X = StandardScaler().fit_transform(X)
-#
-#     # estimate bandwidth for mean shift
-#     bandwidth = cluster.estimate_bandwidth(X, quantile=params['quantile'])
-#
-#     # connectivity matrix for structured Ward
-#     connectivity = kneighbors_graph(
-#         X, n_neighbors=params['n_neighbors'], include_self=False)
-#     # make connectivity symmetric
-#     connectivity = 0.5 * (connectivity + connectivity.T)
-#
-#     # ============
-#     # Create cluster objects
-#     # ============
-#     ms = cluster.MeanShift(bandwidth=bandwidth, bin_seeding=True)
-#     two_means = cluster.MiniBatchKMeans(n_clusters=params['n_clusters'])
-#     ward = cluster.AgglomerativeClustering(
-#         n_clusters=params['n_clusters'], linkage='ward',
-#         connectivity=connectivity)
-#     spectral = cluster.SpectralClustering(
-#         n_clusters=params['n_clusters'], eigen_solver='arpack',
-#         affinity="nearest_neighbors")
-#     dbscan = cluster.DBSCAN(eps=params['eps'])
-#     affinity_propagation = cluster.AffinityPropagation(
-#         damping=params['damping'], preference=params['preference'])
-#     average_linkage = cluster.AgglomerativeClustering(
-#         linkage="average", affinity="cityblock",
-#         n_clusters=params['n_clusters'], connectivity=connectivity)
-#     birch = cluster.Birch(n_clusters=params['n_clusters'])
-#     gmm = mixture.GaussianMixture(
-#         n_components=params['n_clusters'], covariance_type='full')
-#
-#     clustering_algorithms = (
-#         ('MiniBatchKMeans', two_means),
-#         ('AffinityPropagation', affinity_propagation),
-#         ('MeanShift', ms),
-#         ('SpectralClustering', spectral),
-#         ('Ward', ward),
-#         ('AgglomerativeClustering', average_linkage),
-#         ('DBSCAN', dbscan),
-#         ('Birch', birch),
-#         ('GaussianMixture', gmm)
-#     )
-#
-#     for name, algorithm in clustering_algorithms:
-#         t0 = time.time()
-#
-#         # catch warnings related to kneighbors_graph
-#         with warnings.catch_warnings():
-#             warnings.filterwarnings(
-#                 "ignore",
-#                 message="the number of connected components of the " +
-#                 "connectivity matrix is [0-9]{1,2}" +
-#                 " > 1. Completing it to avoid stopping the tree early.",
-#                 category=UserWarning)
-#             warnings.filterwarnings(
-#                 "ignore",
-#                 message="Graph is not fully connected, spectral embedding" +
-#                 " may not work as expected.",
-#                 category=UserWarning)
-#             algorithm.fit(X)
-#
-#         t1 = time.time()
-#         if hasattr(algorithm, 'labels_'):
-#             y_pred = algorithm.labels_.astype(np.int)
-#         else:
-#             y_pred = algorithm.predict(X)
-#
-#         plt.subplot(len(datasets), len(clustering_algorithms), plot_num)
-#         if i_dataset == 0:
-#             plt.title(name, size=18)
-#
-#         colors = np.array(list(islice(cycle(['#377eb8', '#ff7f00', '#4daf4a',
-#                                              '#f781bf', '#a65628', '#984ea3',
-#                                              '#999999', '#e41a1c', '#dede00']),
-#                                       int(max(y_pred) + 1))))
-#         # add black color for outliers (if any)
-#         colors = np.append(colors, ["#000000"])
-#         plt.scatter(X[:, 0], X[:, 1], s=10, color=colors[y_pred])
-#
-#         plt.xlim(-2.5, 2.5)
-#         plt.ylim(-2.5, 2.5)
-#         plt.xticks(())
-#         plt.yticks(())
-#         plt.text(.99, .01, ('%.2fs' % (t1 - t0)).lstrip('0'),
-#                  transform=plt.gca().transAxes, size=15,
-#                  horizontalalignment='right')
-#         plot_num += 1
-#
-# plt.show()
+        # cv_results = model_selection.cross_val_score(model(n_clusters=len(list(set(y)))), X, y, cv=kfold, scoring='accuracy')
+        # #cv_results = model_selection.cross_val_score(model(), X, y, cv=kfold, scoring='homogeneity_score')
+        # results.append(cv_results)
+        # names.append(name)
+        # msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+        # print(msg)
